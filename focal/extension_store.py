60c9768573fa07c2d55596ad092b2b58c1fe1c42
Christian Avart <cavart28@gmail.com>
2022-01-27 16:00:09 -0500

    fix: #2

--- focal/extension_store.py
+++ focal/extension_store.py
@@ -25,7 +25,7 @@ Now we can save each of these in a relevant format:
 >>> d['my_jdict.json'] = my_jdict
 >>> d['my_string.txt'] = my_string
 
-Our folder now contains those files
+Our folder now contains those∑ files
 
 >>> assert set(listdir(temp_dir.name)) == {'my_jdict.json', 'my_string.txt'}
 
@@ -39,7 +39,7 @@ Finally, we clean up the temporary folder
 >>> temp_dir.cleanup()
 
 """
-
+import os
 import pickle
 import json
 from io import BytesIO
@@ -47,7 +47,31 @@ from functools import partial
 from contextlib import suppress
 
 from dol import wrap_kvs, Pipe
-from py2store import LocalBinaryStore
+from dol.filesys import Files
+
+
+# TODO: Empty {} do not work, fix that
+class LocalBinaryStore(Files):
+
+    def __init__(self, path_format, max_levels=None):
+        dirs = path_format.split(os.sep)
+
+        first_bracket_idx = None
+        for idx, dir in enumerate(dirs):
+            if '{' in dir:  # TODO: Replace that hack, may be using regex or string.Formatter
+                first_bracket_idx = idx
+                break
+
+        if first_bracket_idx is not None:
+            rootdir = os.sep.join(dirs[:first_bracket_idx])
+            subpath = os.sep.join(dirs[first_bracket_idx:])
+
+        else:
+            rootdir = path_format
+            subpath = ''
+
+        super().__init__(rootdir=rootdir, subpath=subpath, max_levels=max_levels)
+
 
 # ---------------------------Object to bytes---------------------------------------------------------------------------
 
@@ -73,28 +97,32 @@ extensions_preset_postget = {
 with suppress(ModuleNotFoundError):
     import numpy as np
 
+
     def array_to_bytes(arr: np.ndarray) -> bytes:
         np_bytes = BytesIO()
         np.save(np_bytes, arr)
         return np_bytes.getvalue()
+
 
     bytes_to_array = Pipe(BytesIO, np.load)
     extensions_preset_postget.update(
         {'npy': {'preset': array_to_bytes, 'postget': bytes_to_array}}
     )
 
-
 with suppress(ModuleNotFoundError):
     import pandas as pd
 
+
     def df_to_csv_bytes(df: pd.DataFrame, format='utf-8', index=False):
         return bytes(df.to_csv(index=index), format)
+
 
     def df_to_xlsx_bytes(df: pd.DataFrame, byte_to_file_func=BytesIO):
         towrite = byte_to_file_func()
         df.to_excel(towrite, index=False)
         towrite.seek(0)
         return towrite.getvalue()
+
 
     csv_bytes_to_df = Pipe(BytesIO, pd.read_csv)
     excel_bytes_to_df = Pipe(BytesIO, pd.read_excel)
